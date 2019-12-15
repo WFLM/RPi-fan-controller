e391574aa8142cc76980c0f5579f21f2d693c71c
Yury Kuznetsov <yurykuznetsov@protonmail.com>
2019-12-15 17:47:26 +0300

    add fan control using PWM

--- main.py
+++ main.py
@@ -4,6 +4,14 @@
 #  10.12.2019  20:45
 
 
+FAN_GPIO = 4
+PWM_FREQUENCY_HZ = 20
+
+
+import pigpio
+from simple_pid import PID
+
+
 class CPUTemperatureSensor:
     def __init__(self):
         cpu_temp_sensor_file_path = "/sys/class/thermal/thermal_zone0/temp"
@@ -17,3 +25,34 @@ class CPUTemperatureSensor:
 
     def __del__(self):
         self.temp_descriptor.close()
+
+
+class FanControl:
+    def __init__(self, ip=None, port=None):
+        if port and ip:
+            self.pi = pigpio.pi(ip, port)
+        else:
+            self.pi = pigpio.pi()
+
+        self.pi.set_mode(FAN_GPIO, pigpio.OUTPUT)
+        self.pi.set_PWM_frequency(FAN_GPIO, PWM_FREQUENCY_HZ)
+        self.pi.set_PWM_range(FAN_GPIO, 100)
+
+        self._duty = 0
+
+    @property
+    def duty(self):
+        return self._duty
+
+    @duty.setter
+    def duty(self, value):
+        if value < 0:
+            self._duty = 0
+        elif value > 100:
+            self._duty = 100
+        else:
+            self._duty = value
+        self.pi.set_PWM_dutycycle(FAN_GPIO, PWM_FREQUENCY_HZ)
+
+    def __del__(self):
+        self.pi.stop()
